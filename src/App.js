56fbe9ec4ed8e3001ecb7f4a367666394ad2eb9e
Akshay Yadav <akshayhat@gmail.com>
2022-09-25 15:00:57 +0530

    create note complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import { useState } from 'react';
 import './App.css';
 import Createnote from './components/Createnote';
 import Notes from './components/Notes';
 import Searchbox from './components/Searchbox';
 
 function App() {
+  // const [notes, setNotes] = useState({})
+  const [arr, setArr] = useState([])
+  const [nid, setNid] = useState(0)
+
+  const setnotesobj = (title, text) => {
+    console.log("in app setnotesobj");
+    let notes = { nid, title, text }
+    let narr = arr.slice();
+    narr.push(notes)
+    setArr(narr)
+  }
+
+  console.log(arr);
+
   return (
     <>
       <div className='header'>
-        <Createnote />
+        <Createnote setnotesobj={setnotesobj} nid={nid} />
         <Searchbox />
       </div>
       <Notes />
--- src/components/Createnote.jsx
+++ src/components/Createnote.jsx
@@ -1,8 +1,18 @@
 import React, { useState } from 'react'
 import { Button, Form, Modal } from 'react-bootstrap'
 
-function Createnote() {
+function Createnote(props) {
   const [show, setShow] = useState(false);
+  const [title, setTitle] = useState('')
+  const [text, setText] = useState()
+
+
+  const setnotesObj = () => {
+    setShow(false)
+
+    console.log("in createnote");
+    props.setnotesobj(title, text);
+  }
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -16,16 +26,16 @@ function Createnote() {
         <Modal.Body>
           <Form>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Control type="text" placeholder="Title" autoFocus />
+              <Form.Control type="text" placeholder="Title" autoFocus onChange={(e) => setTitle(e.target.value)} />
             </Form.Group>
             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-              <Form.Control as="textarea" rows={3} placeholder="Enter text here" />
+              <Form.Control as="textarea" rows={3} placeholder="Enter text here" onChange={(e) => setText(e.target.value)} />
             </Form.Group>
           </Form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>Close</Button>
-          <Button variant="primary" onClick={handleClose}>Save Changes</Button>
+          <Button variant="primary" onClick={setnotesObj} >Save Changes</Button>
         </Modal.Footer>
       </Modal>
     </>
